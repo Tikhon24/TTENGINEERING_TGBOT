2182f527ebc8c122c6f3c14d37cf4c36f628ed25
Egor <andreyev.mregor@gmail.com>
2025-07-30 19:07:18 +0500

    catalog 0.1.1

--- app/handlers.py
+++ app/handlers.py
@@ -1,10 +1,18 @@
 from aiogram import Router, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 router = Router()
+
+
+class Catalog(StatesGroup):
+    table = State()
+    parameters = State()
+    parameters_list = {}
 
 
 @router.message(CommandStart())
@@ -14,7 +22,31 @@ async def start(massage: Message):
 
 @router.message(F.text == "Каталог")
 @router.message(Command("catalog"))
-async def open_catalog(massage: Message):
-    await massage.answer("*каталог*")
+async def open_catalog(massage: Message, state: FSMContext):
+    tables = ["1", "2", "3", "4"]  # запрос на получение всех названий таблиц
+    await state.set_state(Catalog.table)
+
+    await massage.answer("*каталог*", reply_markup=await kb.create_keyboard("tables", tables))
+
+
+@router.callback_query(lambda c: c.data.startswith('tables:'))
+async def choose_table(callback: CallbackQuery, state: FSMContext):
+    data = callback.data.split(':')
+    table = data[1]
+    await state.update_data(table=table)
+    parameter = [{"key": ["1", "2"]}, 3]  # зпрос на получение первого вопроса
+    options = parameter[0]
+    quantity = parameter[1]
+    await callback.message.edit_text(text="опа", reply_markup=await kb.create_keyboard("parameters", options, quantity))
+
+
+@router.callback_query(lambda c: c.data.startswith('parameters:'))
+async def choose_options(callback: CallbackQuery, state: FSMContext):
+    data = callback.data.split(':')
+    option = data[1]
+    quantity = data[2]
+    quantity -= 1
+    if quantity > 0:
+
 
 
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,7 +1,15 @@
 from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 start = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Каталог")],
     [KeyboardButton(text="О нас"), KeyboardButton(text="Помощь")]
 ])
+
+
+async def create_keyboard(general_callback, buttons, other_data=None):
+    keyboard = InlineKeyboardBuilder()
+    for button in buttons:
+        keyboard.add(InlineKeyboardButton(text=button, callback_data=f"{general_callback}:{button}:{other_data}"))
+    return keyboard.adjust(1).as_markup()
 

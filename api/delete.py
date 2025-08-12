2948920e2d63cf1fb0e8012c39a81eb7feac45f3
Tikhon24 <timihailoff@yandex.ru>
2025-08-13 01:52:49 +0700

    delete.py

---
+++ api/delete.py
@@ -0,0 +1,19 @@
+from api.settings import TableSettings as table_settings
+from database.tools.master import DataBaseMaster
+
+
+class DeleteMaster:
+    """
+    Ведет удаление техники из бд
+    """
+
+    @staticmethod
+    async def first_request() -> list[str]:
+        """ Возвращает названия таблиц из базы данных """
+        tables_names = await table_settings.get_tables_name()
+        return tables_names
+
+    @staticmethod
+    async def delete_request(table: str, name: str):
+        master = DataBaseMaster(table_settings.MODELS[table])
+        await master.delete_by_name(name)
